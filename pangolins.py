9c9c558ea629036e2219a4b2d224d07aed6ef8b0
Edwin Young <syoung@atlassian.com>
2021-12-06 21:13:42 +1100

    refactoring

--- pangolins.py
+++ pangolins.py
@@ -1,139 +1,137 @@
 import sys
 import typing
 
-
-class Node(object):
-    def __init__(self, text: str, parent):
-        self.text = text
-        self.parent = parent
-        self.yes = None
-        self.no = None
-
+# Use the Composite design pattern to maintain 
+# a hierarchy of questions with answers as the leaf nodes.
+class Node:
     def is_leaf(self) -> bool:
-        if self.yes == None and self.no == None:
-            return True
-        else:
-            return False
+        return isinstance(self, Animal)
+
+class Question(Node):
+    def __init__(self, question: str):
+        self.text = question
+        self.children = []
+
+    def getText(self):
+        return self.text
+    
+    def add(self, node:Node):
+        self.children.append(node)
+
+    def getChild(self, i:int):
+        if i < 0 or i > (len(self.children) - 1):
+            raise IndexError()
+        return self.children[i]
+
+    def getNumChildren(self):
+        return len(self.children)
+
+    def print(self):
+        print("Question: {}".format(self.text))
+        for node in self.children:
+            node.print()
 
     def __str__(self):
         return self.text
 
 
-def get_yes_no_answer(question: str) -> str:
+class Animal(Node):
+    def __init__(self, animal:str):
+        self.text = animal
+
+    def getText(self):
+        return self.text
+
+    def print(self):
+        print("Animal: {}".format(self.text))
+
+    def __str__(self):
+        return self.text
+
+def prompt_yes_no_answer(question: str) -> str:
     while True:
         print("{} [y/n] ".format(question))
         ans = sys.stdin.readline().strip()
         if ans == "t":
-            print_tree(root)
+            root.print()
         if ans == "y" or ans == "n":
             return ans
 
-# debug
-def print_node(node: Node, tag: str):
-    print("===============================")
-    print("= Node text: ({}) {}".format(tag, node.text))
-    if not node.parent:
-        print("= Parent: None")
-    else:
-        print("= Parent: {}".format(node.parent.text))
-        if node == node.parent.yes:
-            print("= Relationship: yes")
-        elif node == node.parent.no:
-            print("= Relationship: no")
-        else:
-            print("= Relationship: unknown")
-    if node.yes:
-        print("= yes node defined")
-    elif node.no:
-        print("= no node defined")
-    else:
-        print("= This is a leaf node")
 
 
-def print_tree(node: Node, tag: str):
-    print_node(node, tag)
-    if node.yes:
-        print_tree(node.yes, "yes")
-    elif node.no:
-        print_tree(node.no, "no")
+# There are possible outcomes:
+# 1. we guess correctly
+# 2. we guess wrong (the node arg is a Animal)
+#    - we need to insert a new Question node between the Animal and its parent
+# 3. we exhaust our questions (the node arg is Question)
+#    - we need to add another question to the list in the current Node
+#
+# Depending on if the asnwer to the new question is yes or no, we'll either need
+# to add the new animal as a child to the new question, or append the animal the
+# the end of the children list of the current node.
+def add_new_animal(node: Question, guessed_animal: Animal):
+    
+    print("What animal did you think of?")
+    animal = Animal(sys.stdin.readline().strip())
+    print("Can you give me a question that is more specific than:\n"
+            " --  {}? --\n"
+            "to which the answer -- {} -- would be true?".format(
+                node, animal)
+    )
+    new_question = Question(sys.stdin.readline().strip().replace("?", ""))
+    new_question.add(animal)
+    node.add(new_question)
+    print("-------------------------------------------------------------")
+    print("I will try my best to remember a {}.".format(animal))
+    print("-------------------------------------------------------------")
 
-
-def guess(node: Node) -> Node:
+def guess(node: Node, parent: Node) -> Node:
     if not node:
-        return None;
+        return None
+       
+    i = 0
+    while i < node.getNumChildren():
+        curnode = node.getChild(i)
+        if isinstance(curnode, Question):
+            yesno = prompt_yes_no_answer("{}?".format(curnode))
+            if yesno == "y":
+                return guess(curnode, node)
+        if isinstance(curnode, Animal):
+            yesno = prompt_yes_no_answer("Is it a {}?".format(curnode))
+            if yesno == "y":
+                return(curnode)
+        i = i + 1
 
-    if node.is_leaf():
-        return node
+    # at this point we've exhausted the available questions
+    print("I give up!")
+    add_new_animal(node, None)
+    return None
 
-    yesno = get_yes_no_answer(node.text)
-    if yesno == "y" and node.yes:
-        return guess(node.yes)
-    if yesno == "n" and node.no:
-        return guess(node.no)
+if __name__ == "__main__":
 
-    return node
+    root = None
+    done = False
+    points = 0
 
+    # bootstrap the model with some basic cases
+    root = Question("Is it an animal?")
+    q1 = Question("Does it live in the sea?")
+    q1.add(Animal("Tuna fish"))
+    root.add(q1)   
+    root.add(Question("Does it fly?").add(Animal("Cuckatoo")))
+    root.add(Animal("pangolin"))
 
-# in this case the questioning has concluded on a "no" and we need to add a new
-# leaf question and answer.
-def handle_give_up(question: str, answer: str, current_node: Node) -> Node:
-    question_node = Node(question, current_node)
-    current_node.no = question_node
-    question_node.yes = Node(answer, question_node)
-    return question_node
-
-
-# here we want to insert a new node between the parent and the current node.
-# The current node needs to become the negative answer to the
-# new question.
-def handle_wrong_guess(question: str, answer: str, current_node: Node) -> Node:
-    parent = current_node.parent
-
-    new_question_node = Node(question, parent)
-
-    # save old yes answer
-    old_yes = parent.yes
-
-    parent.yes = new_question_node
-
-    new_question_node.no = old_yes
-    new_question_node.yes = Node(answer, new_question_node)
-    return new_question_node
-
-
-root = None
-done = False
-points = 0
-
-# bootstrap the model cause I'm too lazy to handle the empty case
-root = Node("is it a mammal?", None)
-root.yes = Node("Does it have scales?", root)
-root.yes.yes = Node("pangolin", root.yes)
-
-while not done:
-    get_yes_no_answer("Think of an animal... Are you ready?")
-    node = guess(root)
-    if node.is_leaf():
-        yesno = get_yes_no_answer("Oh! Oh! I think I know what it is. Did you think of a... {} ?".format(node.text))
-        if yesno == "y":
+    while not done:
+        if prompt_yes_no_answer("\n\nThink of an animal... Are you ready?") == "n":
+            break
+        node = guess(root, None)
+        if node is not None:
             points = points + 1
             print("1 extra point for me! I now have {} points.".format(points))
             continue
 
-    print("I give up! What is it???")
-    new_animal_str = sys.stdin.readline().strip()
+        if prompt_yes_no_answer("Would you like to go again?") == "n":
+            done = True
 
-    print('Give me a yes/no type question for which the answer would be yes for {}:'.format(new_animal_str))
-    question = sys.stdin.readline().strip()
+    print("Thanks for playing!")
 
-    if node.is_leaf():
-        handle_wrong_guess(question, new_animal_str, node)
-    else:
-        handle_give_up(question, new_animal_str, node)
-
-    print("I will try to remember a {}.".format(new_animal_str))
-
-    if get_yes_no_answer("Would you like to go again?") == "n":
-        done = True
-
-print("Thanks for playing!")
